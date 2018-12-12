d3d07335c1cec2867a0aa63a49ea796444bb96e7
misha <mike008@bk.ru>
2018-12-13 00:45:28 +0300

    + full no testing

--- main.cpp
+++ main.cpp
@@ -5,19 +5,66 @@
 #include <fstream>
 #include <vector>
 #include <cmath>
+#include <algorithm>
+
+std::vector<std::pair<int, int>> v;
+std::vector<std::vector<std::pair<int, int>>> sectors_dist_ind_v;
+std::vector<std::pair<int, std::pair<int, int>>> rib_cost_ind_v1_v2;
 
 int dist(std::pair<int, int>& v1, std::pair<int, int>& v2)
 {
     return abs(v1.first - v2.first) + abs(v1.second - v2.second);
 }
 
+void add_rib(int& ind_v1, int& ind_v2)
+{
+    rib_cost_ind_v1_v2.emplace_back();
+    rib_cost_ind_v1_v2.back().second.first = ind_v1;
+    rib_cost_ind_v1_v2.back().second.second = ind_v2;
+    rib_cost_ind_v1_v2.back().first = dist(v[ind_v1], v[ind_v2]);
+
+}
+
+int move_last_v_from_sector(int init_ind_v, int ind_sector)
+{
+    int result = sectors_dist_ind_v[ind_sector].back().second;
+    add_rib(init_ind_v, sectors_dist_ind_v[ind_sector].back().second);
+    sectors_dist_ind_v[ind_sector].pop_back();
+    return result;
+}
+
+void f(int init_ind_v, int ind_begin_sector, int ind_end_sector) {
+    if (ind_begin_sector == ind_end_sector)
+    {
+        int pred_ind = init_ind_v;
+        for(unsigned long i = sectors_dist_ind_v[ind_begin_sector].size() - 1; i != 0; i--)
+        {
+            move_last_v_from_sector(pred_ind, ind_begin_sector);
+            pred_ind = sectors_dist_ind_v[ind_begin_sector][i].second;
+        }
+        return;
+    }
+    int ind_sector_min = ind_end_sector;
+    for(int i = ind_begin_sector; i < ind_end_sector; i++)
+    {
+        if(dist(v[init_ind_v], v[sectors_dist_ind_v[i].back().second])
+        < dist(v[init_ind_v], v[sectors_dist_ind_v[ind_sector_min].back().second]))
+            ind_sector_min = i;
+    }
+
+    init_ind_v = move_last_v_from_sector(init_ind_v, ind_sector_min);
+
+    f(init_ind_v, ind_begin_sector, (ind_begin_sector + ind_end_sector) / 2);
+    f(init_ind_v, (ind_begin_sector + ind_end_sector) / 2 + 1, ind_end_sector);
+}
+
+
+
 int main()
 {
     std::fstream in;
     in.open("../input/input.txt");
     int n;
-    std::vector<std::pair<int, int>> v;
-    std::vector<std::vector<std::pair<int, int>>> sectors_ind_v__dist;
 
     std::pair<int, int> p_min, p_max, p_center;
 //    ... чтение графа ...
@@ -49,8 +96,8 @@ int main()
     }
     in.close();
 
-    p_center.first = (p_min.first - p_max.first) / 2;
-    p_center.second = (p_min.second - p_max.second) / 2;
+    p_center.first = (p_min.first + p_max.first) / 2;
+    p_center.second = (p_min.second + p_max.second) / 2;
 
     int min_dist = dist(v[0], p_center);
     int ind_v_center = 0;
@@ -70,10 +117,16 @@ int main()
         l <<= 1;
     }
     l >>= 1;
-    sectors_ind_v__dist.resize(static_cast<unsigned long>(l));
+    sectors_dist_ind_v.resize(static_cast<unsigned long>(l));
 
     double phi_sector = 2*M_PI/l;
 
+//    debug
+    l = 8;
+    sectors_dist_ind_v.clear();
+    sectors_dist_ind_v.resize(static_cast<unsigned long>(l));
+    phi_sector = 2*M_PI/l;
+//--------------
     for(int i = 0; i < n; i++)
     {
         if(i == ind_v_center)
@@ -99,7 +152,12 @@ int main()
             phi_v += M_PI*2;
         }
         auto ind_sector = static_cast<int>(phi_v / phi_sector);
-        sectors_ind_v__dist[ind_sector].push_back(std::pair<int, int>(i, dist(v[i], v[ind_v_center])));
+        sectors_dist_ind_v[ind_sector].push_back(std::pair<int, int>(dist(v[i], v[ind_v_center]), i));
+    }
+
+    for(auto &sector : sectors_dist_ind_v)
+    {
+        std::sort(sector.rbegin(), sector.rend());
     }
 
 
