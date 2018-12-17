cd9c802d4568d3819e785ad3d08a8179935c47cf
misha <mike008@bk.ru>
2018-12-18 01:17:35 +0300

    end result

--- main.cpp
+++ main.cpp
@@ -14,7 +14,7 @@ std::vector<std::pair<int, int>> v;
  */
 std::vector<std::vector<std::pair<int, int>>> sectors;
 /**
- pair<cost, pair<ind_v1, ind_v2>
+ cost, {ind_v1, ind_v2}
  */
 std::vector<std::pair<int, std::pair<int, int>>> ribs;
 
@@ -50,26 +50,74 @@ void f(int init_ind_v, int ind_begin_sector, int ind_end_sector) {
         }
         return;
     }
-    int ind_sector_min = ind_end_sector;
-    for(int i = ind_begin_sector; i < ind_end_sector; i++)
+    int ind_sector_min = -1;
+    for(int i = ind_begin_sector; i <= ind_end_sector; i++)
     {
-        if(dist(v[init_ind_v], v[sectors[i].back().second])
-        < dist(v[init_ind_v], v[sectors[ind_sector_min].back().second]))
-            ind_sector_min = i;
+        if(ind_sector_min != -1)
+        {
+            if(sectors[i].empty())
+                continue;
+            if(dist(v[init_ind_v], v[sectors[i].back().second])
+                < dist(v[init_ind_v], v[sectors[ind_sector_min].back().second]))
+                ind_sector_min = i;
+
+        }
+        else {
+            if(!sectors[i].empty())
+                ind_sector_min = i;
+        }
     }
 
+    if(ind_sector_min == -1)
+        return;
     init_ind_v = move_last_v_from_sector(init_ind_v, ind_sector_min);
 
     f(init_ind_v, ind_begin_sector, (ind_begin_sector + ind_end_sector) / 2);
     f(init_ind_v, (ind_begin_sector + ind_end_sector) / 2 + 1, ind_end_sector);
 }
 
+void f_main(int ind_v_center, int l, double phi_sector) {
+    cost = 0;
+    ribs.clear();
+    for (auto &s : sectors)
+        s.clear();
+
+    for (int j = 0; j < v.size(); j++) {
+        if (j == ind_v_center)
+            continue;
+        double phi_v;
+        int x = v[j].first - v[ind_v_center].first, y = v[j].second - v[ind_v_center].second;
+        if (x == 0) {
+            if (y > 0)
+                phi_v = M_PI / 2;
+            else
+                phi_v = 3 * M_PI / 2;
+        } else if (x < 0) {
+            phi_v = std::atan(double(y) / double(x)) + M_PI;
+        } else {
+            phi_v = std::atan(double(y) / double(x));
+        }
+        if (phi_v < 0) {
+            phi_v += M_PI * 2;
+        }
+        auto ind_sector = static_cast<int>(phi_v / phi_sector);
+        sectors[ind_sector].push_back(std::pair<int, int>(dist(v[j], v[ind_v_center]), j));
+    }
+
+    for (auto &sector : sectors) {
+        std::sort(sector.rbegin(), sector.rend());
+    }
+
+    int step_tree = l / 3;
+    for (int j = 0; j < l; j += step_tree) {
+        f(ind_v_center, j, j + step_tree - 1);
+    }
+}
 
 
 int main()
 {
     std::fstream in;
-//    in.open("../input/Taxicab_2018.txt");
     in.open("../input/Taxicab_2018.txt");
     int n;
 
@@ -78,45 +126,30 @@ int main()
     in >> n;
     v.emplace_back();
     in >> v.back().first >> v.back().second;
-    p_min.first = v.back().first;
-    p_max.first = v.back().first;
-    p_min.second = v.back().second;
-    p_max.second = v.back().second;
-
+//    p_min.first = v.back().first;
+//    p_max.first = v.back().first;
+//    p_min.second = v.back().second;
+//    p_max.second = v.back().second;
+//
     for(int i = 1; i < n; i++)
     {
         v.emplace_back();
         in >> v.back().first >> v.back().second;
-
-        if(p_min.first > v.back().first)
-            p_min.first = v.back().first;
-
-        if(p_max.first < v.back().first)
-            p_max.first = v.back().first;
-
-        if(p_min.second > v.back().second)
-            p_min.second = v.back().second;
-
-        if(p_max.second < v.back().second)
-            p_max.second = v.back().second;
-
+//
+//        if(p_min.first > v.back().first)
+//            p_min.first = v.back().first;
+//
+//        if(p_max.first < v.back().first)
+//            p_max.first = v.back().first;
+//
+//        if(p_min.second > v.back().second)
+//            p_min.second = v.back().second;
+//
+//        if(p_max.second < v.back().second)
+//            p_max.second = v.back().second;
+//
     }
     in.close();
-
-    p_center.first = (p_min.first + p_max.first) / 2;
-    p_center.second = (p_min.second + p_max.second) / 2;
-
-    int min_dist = dist(v[0], p_center);
-    int ind_v_center = 0;
-
-    for(int i = 1; i < v.size(); i++)
-    {
-        if(dist(v[i], p_center) < min_dist)
-        {
-            min_dist = dist(v[i], p_center);
-            ind_v_center = i;
-        }
-    }
 
     int l_max = n / 10, l = 3;
     while(l < l_max)
@@ -128,56 +161,35 @@ int main()
 
     double phi_sector = 2*M_PI/l;
 
-//    debug
-//    l = 8;
-//    sectors.clear();
-//    sectors.resize(static_cast<unsigned long>(l));
-//    phi_sector = 2*M_PI/l;
-//--------------
-    for(int i = 0; i < n; i++)
-    {
-        if(i == ind_v_center)
-            continue;
-        double phi_v;
-        int x = v[i].first - v[ind_v_center].first, y = v[i].second - v[ind_v_center].second;
-        if(x == 0)
-        {
-            if(y > 0)
-                phi_v = M_PI/2;
-            else
-                phi_v = 3*M_PI/2;
+    f_main(0, l, phi_sector);
+    int cost_min = cost, ind_v_min = 0;
+
+    for (int j = 1; j < v.size(); ++j) {
+        f_main(j, l, phi_sector);
+        if (cost < cost_min) {
+            cost_min = cost;
+            ind_v_min = j;
         }
-        else
-            if(x < 0) {
-                phi_v = std::atan(double(y) / double(x)) + M_PI;
-            }
-            else {
-                phi_v = std::atan(double(y) / double(x));
-            }
-        if(phi_v < 0)
-        {
-            phi_v += M_PI*2;
-        }
-        auto ind_sector = static_cast<int>(phi_v / phi_sector);
-        sectors[ind_sector].push_back(std::pair<int, int>(dist(v[i], v[ind_v_center]), i));
     }
 
-    for(auto &sector : sectors)
+    f_main(ind_v_min, l, phi_sector);
+
+    std::vector<int> pow_v(v.size(), 0);
+    for(auto &i : ribs)
     {
-        std::sort(sector.rbegin(), sector.rend());
+        pow_v[i.second.first] += 1;
+        pow_v[i.second.second] += 1;
     }
-//debug
-//    f(ind_v_center, 0, (l - 1)/2);
-//    f(ind_v_center, (l - 1)/2+1, l - 1);
-//------------------
-    int step_tree = l/3;
-    for(int i = 0; i < l; i += step_tree)
+    int count_leaf = 0;
+    for(auto &i : pow_v)
     {
-        f(ind_v_center, i, i + step_tree - 1);
+        if(i == 1)
+            count_leaf += 1;
     }
+
 
     std::fstream result("../result_2018.txt");
-    result << "c Вес дерева = " << cost << ", число листьев = " << l << ',' << std::endl;
+    result << "c Вес дерева = " << cost << ", число листьев = " << count_leaf << ',' << std::endl;
     result << "c число вершин и ребер" << std::endl;
     result << "p edge " << n << ' ' << ribs.size() << std::endl;
     result << "c ребра" << std::endl;
